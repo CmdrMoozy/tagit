03b2102f1010f10d2ba20dbe9ecd11b410970bfc
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-05-08 21:05:17 -0600

    Add a class for encapsulating ALAC files.

---
+++ src/tagitcommon/audio/ALACFile.cpp
@@ -0,0 +1,52 @@
+/*
+ * TagIt - An app for transcoding, tagging, and organizing media files.
+ * Copyright (C) 2015  Axel Rasmussen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ALACFile.h"
+
+#include <taglib/mp4file.h>
+
+#include "tagitcommon/audio/Utils.h"
+#include "tagitcommon/tag/TaggedFile.h"
+
+namespace tagit
+{
+namespace audio
+{
+boost::optional<ALACFile>
+ALACFile::factory(const io::MemoryMappedFile &memoryFile)
+{
+	// If the given file isn't an MP4 file, it can't be an ALAC file.
+	if(!utils::isMP4AudioFile(memoryFile))
+		return boost::none;
+
+	// Use TagLib to differentiate AAC versus ALAC.
+	tagit::tag::TaggedFile<TagLib::MP4::File> tag(memoryFile);
+	if(tag.get().audioProperties()->codec() !=
+	   TagLib::MP4::Properties::ALAC)
+	{
+		return boost::none;
+	}
+
+	return ALACFile();
+}
+
+ALACFile::ALACFile()
+{
+}
+}
+}
