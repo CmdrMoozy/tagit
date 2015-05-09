4c34e325e27d8fbee606c12757cd85cf60bd002f
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-05-09 14:25:08 -0600

    Add a widget providing path input functionality.

---
+++ src/tagitcommon/ui/PathInputWidget.cpp
@@ -0,0 +1,139 @@
+/*
+ * TagIt - An app for transcoding, tagging, and organizing media files.
+ * Copyright (C) 2015  Axel Rasmussen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PathInputWidget.h"
+
+#include <utility>
+
+#include <QDir>
+#include <QFileDialog>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+
+namespace tagit
+{
+namespace ui
+{
+PathInput::PathInput(const std::string &n, PathInputType t, const QString &l)
+        : name(n), type(t), label(l)
+{
+}
+
+PathInputWidget::PathInputWidget(const std::initializer_list<PathInput> &inputs,
+                                 QWidget *p, Qt::WindowFlags f)
+        : QWidget(p, f),
+          layout(nullptr),
+          inputTypes(),
+          buttonInputs(),
+          pathInputs()
+{
+	layout = new QGridLayout(this);
+
+	int i = 0;
+	for(const auto &input : inputs)
+	{
+		// Skip inputs with duplicate names.
+		if(pathInputs.find(input.name) != pathInputs.end())
+			continue;
+
+		QLabel *label = new QLabel(input.label, this);
+		QLineEdit *pathInput = new QLineEdit(this);
+		QPushButton *browseButton =
+		        new QPushButton(tr("Browse..."), this);
+
+		inputTypes.insert(std::make_pair(browseButton, input.type));
+		buttonInputs.insert(std::make_pair(browseButton, pathInput));
+		pathInputs.insert(std::make_pair(input.name, pathInput));
+
+		layout->addWidget(label, i, 0, 1, 1);
+		layout->addWidget(pathInput, i, 1, 1, 1);
+		layout->addWidget(browseButton, i, 2, 1, 1);
+		++i;
+
+		QObject::connect(browseButton, SIGNAL(clicked(bool)), this,
+		                 SLOT(doBrowse()));
+	}
+
+	layout->setColumnStretch(1, 1);
+	layout->setRowStretch(i + 1, 1);
+	setLayout(layout);
+}
+
+PathInputWidget::PathInputWidget(const QString &l, PathInputType t, QWidget *p,
+                                 Qt::WindowFlags f)
+        : PathInputWidget({PathInput("", t, l)}, p, f)
+{
+}
+
+QString PathInputWidget::getPath(const std::string &name) const
+{
+	if(pathInputs.size() == 0)
+		return QString();
+	else if(pathInputs.size() == 1)
+		return pathInputs.begin()->second->text();
+
+	auto it = pathInputs.find(name);
+	if(it == pathInputs.end())
+		return QString();
+	return it->second->text();
+}
+
+void PathInputWidget::doBrowse()
+{
+	QPushButton *button = dynamic_cast<QPushButton *>(sender());
+	if(button == nullptr)
+		return;
+
+	auto tit = inputTypes.find(button);
+	if(tit == inputTypes.end())
+		return;
+	PathInputType type = tit->second;
+
+	auto iit = buttonInputs.find(button);
+	if(iit == buttonInputs.end())
+		return;
+	QLineEdit *input = iit->second;
+
+	switch(type)
+	{
+	case PathInputType::FILE:
+	{
+		QString file = QFileDialog::getOpenFileName(
+		        this, tr("Open File"), QDir::homePath());
+		if(!file.isNull())
+			input->setText(file);
+	}
+	break;
+
+	case PathInputType::DIRECTORY:
+	{
+		QString directory = QFileDialog::getExistingDirectory(
+		        this, tr("Open Directory"), QDir::homePath());
+		if(!directory.isNull())
+			input->setText(directory);
+	}
+	break;
+
+	default:
+		break;
+	};
+}
+}
+}
---
+++ src/tagitcommon/ui/PathInputWidget.h
@@ -0,0 +1,113 @@
+/*
+ * TagIt - An app for transcoding, tagging, and organizing media files.
+ * Copyright (C) 2015  Axel Rasmussen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TAGITCOMMON_UI_PATH_INPUT_WIDGET_H
+#define TAGITCOMMON_UI_PATH_INPUT_WIDGET_H
+
+#include <initializer_list>
+#include <map>
+
+#include <QString>
+#include <QWidget>
+
+class QGridLayout;
+class QLabel;
+class QLineEdit;
+class QPushButton;
+
+namespace tagit
+{
+namespace ui
+{
+/*!
+ * \brief Denotes the type of path a PathInputWidget should select.
+ */
+enum class PathInputType
+{
+	FILE,
+	DIRECTORY
+};
+
+/*!
+ * \brief This structure defines a path input's characteristics.
+ */
+struct PathInput
+{
+	std::string name;
+	PathInputType type;
+	QString label;
+
+	PathInput(const std::string &n, PathInputType t, const QString &l);
+
+	PathInput(const PathInput &) = default;
+	PathInput &operator=(const PathInput &) = default;
+};
+
+/*!
+ * \brief This widget provides a standard text / browse path input widget.
+ */
+class PathInputWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	/*!
+	 * \param inputs The list of path inputs to create.
+	 * \param p This widget's parent widget.
+	 * \param f The window flags for this widget.
+	 */
+	PathInputWidget(const std::initializer_list<PathInput> &inputs,
+	                QWidget *p = nullptr, Qt::WindowFlags f = 0);
+
+	/*!
+	 * \param l The label for this path input widget.
+	 * \param t The type of path to input.
+	 * \param p This widget's parent widget.
+	 * \param f The window flags for this widget.
+	 */
+	PathInputWidget(const QString &l, PathInputType t, QWidget *p = nullptr,
+	                Qt::WindowFlags f = 0);
+
+	PathInputWidget(const PathInputWidget &) = delete;
+	virtual ~PathInputWidget() = default;
+
+	PathInputWidget &operator=(const PathInputWidget &) = delete;
+
+	/*!
+	 * This function returns the currently selected path from a single
+	 * path input widget. If this widget only contains one input, then
+	 * that input's path is returned regardless of the name provided.
+	 *
+	 * \param The input name to retrieve the path from.
+	 * \return This widget's currently selected path, as a string.
+	 */
+	QString getPath(const std::string &name = "") const;
+
+private:
+	QGridLayout *layout;
+	std::map<const QPushButton *, PathInputType> inputTypes;
+	std::map<const QPushButton *, QLineEdit *> buttonInputs;
+	std::map<std::string, QLineEdit *> pathInputs;
+
+private Q_SLOTS:
+	void doBrowse();
+};
+}
+}
+
+#endif
